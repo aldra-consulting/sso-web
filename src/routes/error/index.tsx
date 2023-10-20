f2768efb6abc864b88221a0845a917c3befc5753
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-10-20 13:41:19 +0200

    feat: add error page

---
+++ src/routes/error/index.tsx
@@ -0,0 +1,25 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { type DocumentHead } from '@builder.io/qwik-city';
+
+import styles from './styles.css?inline';
+
+export default component$(() => {
+  useStylesScoped$(styles);
+
+  return (
+    <main id='page'>
+      <span>Something went wrong!</span>
+    </main>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'ID | Aldra | IT-spesialister i verdensklasse',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Aldra er et norsk konsulenthus som bistår bedrifter med å finne smarte løsninger til morgendagends utfordringer.',
+    },
+  ],
+};
