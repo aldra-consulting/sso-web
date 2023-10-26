94424dbc93b79209246ab573fc7be7b7880cec96
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-10-26 17:50:07 +0200

    feat: improve error route

--- src/routes/error/index.tsx
+++ src/routes/error/index.tsx
@@ -1,15 +1,40 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
 
+import Animated from '../../components/animated';
+import Button from '../../components/button';
+import Centered from '../../components/centered';
+import Logo from '../../components/logo';
+import { useAppNavigation } from '../../hooks';
+
 import styles from './styles.css?inline';
 
 export default component$(() => {
   useStylesScoped$(styles);
 
+  const { goToSignIn } = useAppNavigation();
+
   return (
-    <main id='page'>
-      <span>Something went wrong!</span>
-    </main>
+    <Centered>
+      <Animated animation='fade-in-up'>
+        <div id='layout'>
+          <div id='logo'>
+            <Logo height={34} />
+          </div>
+          <h1>Noe gikk galt</h1>
+          <h2>Det har oppstått en uventet feil</h2>
+          <div id='actions-slot'>
+            <Button
+              variant='contained'
+              colour='blue'
+              onClick$={() => goToSignIn()}
+            >
+              Prøv igjen
+            </Button>
+          </div>
+        </div>
+      </Animated>
+    </Centered>
   );
 });
 
